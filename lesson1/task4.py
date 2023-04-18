7a5360919e5fa56913d69ad7bba0b1227836e746
Starunov <35844513@bk.ru>
2023-04-18 19:01:47 +0300

    homework6

--- lesson1/task4.py
+++
@@ -1,9 +0,0 @@
-from f import center
-
-words = ['разработка', 'администрирование', 'protocol', 'standard']
-
-print(center('Строковое представление', 'Обратное преобразование', 'Байтовое представление', header=True))
-for word in words:
-    we = word.encode('utf-8')
-    wd = we.decode('utf-8')
-    print(center(word, wd, we))
